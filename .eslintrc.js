65d99039c11787eec6fa535405255b6892c83093
vishwagraphic <vishwa.graphic@gmail.com>
2021-04-21 19:20:20 -0400

    changes to dir

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
       'plugin:prettier/recommended',
     ],
     rules: {
-      'no-unused-vars': 'off',
+      'no-unused-vars': 'on',
       '@typescript-eslint/no-var-requires': 'off',
       'react/prop-types': 'off',
       'react/jsx-uses-react': 'off',
